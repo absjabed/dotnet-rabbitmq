1e7f2b286022d1979d37e77d036343ab0033c3de
MD Abu Bakar Siddqe Jabed <abs.jabed@kdsgroup.net>
2021-04-13 15:47:39 +0600

    init RabbitMQ Single Producer & Single Consumer...

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+             var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@192.168.99.100:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            /**
+                ONCE CHANNEL IS CREATED WE DONT NEED BELOW LINE
+                TO CREATE NEW CHANNEL..AS IT'S ALREADY BEEN CREATED.
+            **/
+            // channel.QueueDeclare("demo-queue-kds",
+            //         durable: true,
+            //         exclusive: false,
+            //         autoDelete: false,
+            //         arguments: null);
+            
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) => {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                 Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue-kds", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Threading;
+
+namespace RabbitMQ.Producer
+{
+   static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@192.168.99.100:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue-kds",
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            for(var i = 1; i<=1000; i++ ){
+
+                 Thread.Sleep(100);
+
+                    var message = new {Count = "a"+i};
+                    //new { Name = "From Producer", Message = "Hello! "+ i};
+                    var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                    channel.BasicPublish("", "demo-queue-kds", null, body);
+
+                Thread.Yield();
+            }
+        }
+    }
+}
